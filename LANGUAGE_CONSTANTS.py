868f4ff38672ceb8c2c6da6d2c6112401c68dde0
ddeepak95 <ddeepak95@gmail.com>
2025-11-10 12:35:01 -0500

    add hindi support

--- LANGUAGE_CONSTANTS.py
+++ LANGUAGE_CONSTANTS.py
@@ -4,4 +4,5 @@ LANGUAGES = {
     "ta": {"pipecat_language": Language.TA, "name": "Tamil", "cartesia_voice_id": "7f98e662-142d-41ba-89a2-12452640ce6d"},
     "en": {"pipecat_language": Language.EN, "name": "English", "cartesia_voice_id": "8d8ce8c9-44a4-46c4-b10f-9a927b99a853"},
     "de": {"pipecat_language": Language.DE, "name": "German", "cartesia_voice_id": "1ade29fc-6b82-4607-9e70-361720139b12"},
+    "hi": {"pipecat_language": Language.HI, "name": "Hindi", "cartesia_voice_id": "95d51f79-c397-46f9-b49a-23763d3eaa2d"},
 }
